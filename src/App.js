e8dc5e372f6a9a3fbbb21f13dac94910026f4c52
SRINU-NAYAK <srinukto@gmail.com>
2025-01-13 22:03:56 +0000

    developed other components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,59 @@
-import {Route, Switch, Redirect} from 'react-router-dom'
-
-import Home from './components/Home'
+import {Component} from 'react'
+import {Switch, Route} from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
 import Login from './components/Login'
-// import Header from './components/Header'
-import BookDetails from './components/BookDetails'
+import Home from './components/Home'
 import BookShelf from './components/BookShelf'
+import MyFavorite from './components/MyFavorite'
+import BookDetails from './components/BookDetails'
+import FavoriteContext from './Context/FavoriteContext'
 import NotFound from './components/NotFound'
+
 import './App.css'
 
-// use the below bookshelvesList for rendering read status of book items in Bookshelves Route
+class App extends Component {
+  state = {favoriteList: []}
 
-// const bookshelvesList = [
-//   {
-//     id: '22526c8e-680e-4419-a041-b05cc239ece4',
-//     value: 'ALL',
-//     label: 'All',
-//   },
-//   {
-//     id: '37e09397-fab2-46f4-9b9a-66b2324b2e22',
-//     value: 'READ',
-//     label: 'Read',
-//   },
-//   {
-//     id: '2ab42512-3d05-4fba-8191-5122175b154e',
-//     value: 'CURRENTLY_READING',
-//     label: 'Currently Reading',
-//   },
-//   {
-//     id: '361d5fd4-9ea1-4e0c-bd47-da2682a5b7c8',
-//     value: 'WANT_TO_READ',
-//     label: 'Want to Read',
-//   },
-// ]
+  onToggleFavorite = bookDetails => {
+    const {favoriteList} = this.state
+    const isAlreadyExist = favoriteList.some(
+      eachItem => eachItem.id === bookDetails.id,
+    )
 
-const App = () => (
-  <>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/shelf" component={BookShelf} />
-      <Route exact path="/books/:id" component={BookDetails} />
-      <Route exact path="/not-found" component={NotFound} />
-      <Redirect to="not-found" />
-    </Switch>
-  </>
-)
+    if (isAlreadyExist === true) {
+      this.setState(prevState => ({
+        favoriteList: prevState.favoriteList.filter(
+          eachBook => eachBook.id !== bookDetails.id,
+        ),
+      }))
+    } else {
+      this.setState(prevState => ({
+        favoriteList: [...prevState.favoriteList, bookDetails],
+      }))
+    }
+  }
+
+  render() {
+    const {favoriteList} = this.state
+
+    return (
+      <FavoriteContext.Provider
+        value={{
+          favoriteList,
+          onToggleFavorite: this.onToggleFavorite,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/shelf" component={BookShelf} />
+          <ProtectedRoute exact path="/favorite" component={MyFavorite} />
+          <ProtectedRoute exact path="/books/:id" component={BookDetails} />
+          <Route component={NotFound} />
+        </Switch>
+      </FavoriteContext.Provider>
+    )
+  }
+}
 
 export default App
